e4e60a9bb21a93b01454fdd41f5ba7e5732784c6
Garren Miller <garrenkmiller@gmail.com>
2024-11-25 19:35:19 -0700

    Build and flash works with probe-rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
-//! Prints "Hello, world!" on the host console using semihosting
-
-#![no_main]
 #![no_std]
+#![no_main]
 
-use panic_halt as _;
-
+use core::panic::PanicInfo;
+use cortex_m::delay::Delay;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
+use hal::{
+    self,
+    clocks::Clocks,
+    gpio::{Pin, PinMode, Port},
+    pac,
+};
+
+// use defmt_rtt as _; // global logger
+
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
-    loop {}
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut dp = pac::Peripherals::take().unwrap();
+
+    let clock_cfg = Clocks::default();
+
+    clock_cfg.setup(&mut dp.RCC, &mut dp.FLASH).unwrap();
+
+    let mut dealy = Delay::new(cp.SYST, clock_cfg.systick());
+    
+    // TODO: This does not work; have to use the SPI interface
+    let mut led = Pin::new(Port::C, 13, PinMode::Output);
+
+    loop {
+        led.set_low();
+        dealy.delay_ms(1000);
+        led.set_high();
+        dealy.delay_ms(1000);
+    }
+}
+
+// same panicking *behavior* as `panic-probe` but doesn't print a panic message
+// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    cortex_m::asm::udf()
+}
+
+/// Terminates the application and makes `probe-run` exit with exit-code = 0
+pub fn exit() -> ! {
+    loop {
+        cortex_m::asm::bkpt();
+    }
 }
